3aff760a6981ea988cd241e1b53ee447c700912f
MrCronkite <vladshima99@gmail.com>
2022-09-02 22:02:38 +0300

    create pr

---
+++ SwiftPlayer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwiftPlayer
+//
+//  Created by admin1 on 2.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let imageView: UIImageView = {
+        
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
