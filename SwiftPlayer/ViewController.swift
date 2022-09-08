dc597b710b5136e0eec489a47a3cd8a4779921fa
MrCronkite <vladshima99@gmail.com>
2022-09-08 20:21:29 +0300

    new constraint

--- SwiftPlayer/ViewController.swift
+++ SwiftPlayer/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var nameSong: UILabel!
+    @IBOutlet weak var artistName: UILabel!
+    
+    @IBOutlet weak var progressBar: UISlider!
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func playSong(_ sender: Any) {
+    }
+    
+    @IBAction func nextSong(_ sender: Any) {
+    }
+    
+    @IBAction func prevSong(_ sender: Any) {
+    }
 }
